a340bac22ff4789aaf5e907d39da6434486dd2e9
haholm <57066006+haholm@users.noreply.github.com>
2020-05-29 16:05:34 +0200

    Added native functions related to console input

--- Conhics/Integration.cs
+++ Conhics/Integration.cs
@@ -93,7 +93,64 @@ namespace Conhics {
         );
 
         public enum StdHandle {
+            InputHandle = -10,  // Used for mouse input
             OutputHandle = -11
         }
+        // End console output-specific members
+
+        // Begin input-specific members
+        public const int ENABLE_MOUSE_INPUT = 0x0010;
+        public const int ENABLE_QUICK_EDIT_MODE = 0x0040;
+        public const int ENABLE_EXTENDED_FLAGS = 0x0080;
+        public const int KEY_EVENT = 1;
+        public const int MOUSE_EVENT = 2;
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct INPUT_RECORD {
+            [FieldOffset(0)]
+            public short EventType;
+            [FieldOffset(4)]
+            public KEY_EVENT_RECORD KeyEvent;
+            [FieldOffset(4)]
+            public MOUSE_EVENT_RECORD MouseEvent;
+        }
+
+        public struct MOUSE_EVENT_RECORD {
+            public COORD dwMousePosition;
+            public int dwButtonState;
+            public int dwControlKeyState;
+            public int dwEventFlags;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct KEY_EVENT_RECORD {
+            [FieldOffset(0)]
+            [MarshalAs(UnmanagedType.Bool)]
+            public bool bKeyDown;
+            [FieldOffset(4)]
+            public ushort wRepeatCount;
+            [FieldOffset(6)]
+            public ushort wVirtualKeyCode;
+            [FieldOffset(8)]
+            public ushort wVirtualScanCode;
+            [FieldOffset(10)]
+            public char UnicodeChar;
+            [FieldOffset(10)]
+            public byte AsciiChar;
+            [FieldOffset(12)]
+            public int dwControlKeyState;
+        };
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetConsoleMode(IntPtr hConsoleHandle, ref int lpMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadConsoleInput(IntPtr hConsoleInput, ref INPUT_RECORD lpBuffer, uint nLength, ref uint lpNumberOfEventsRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetConsoleMode(IntPtr hConsoleHandle, int dwMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetNumberOfConsoleInputEvents(IntPtr hConsoleInput, out uint lpcNumberOfEvents);
     }
 }
